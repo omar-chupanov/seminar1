3c485661a7dd8de9a1b3ad9cb5d81134b2cb13b0
Omar <omar-chupanov@mail.ru>
2023-03-31 12:35:58 +0300

    homework

---
+++ t2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Укажите число >");
+string value;
+value = Console.ReadLine();
+var numA = Convert.ToInt32(value);
+System.Console.Write("Укажите число >");
+value = Console.ReadLine();
+var numB = Convert.ToInt32(value);
+System.Console.Write("Укажите число >");
+value = Console.ReadLine();
+var numC = Convert.ToInt32(value);
+var max = numA;
+if (max < numB)
+{
+    max = numB;
+    if (max< numC)
+    {
+        max = numC;
+        System.Console.Write("max =" + numC);
+    }
+    if (max>numC)
+    {
+        System.Console.Write("max =" + numB);
+    }
+}
+if (max<numC)
+{
+    System.Console.Write("max =" + numC);
+}
+else 
+{
+    System.Console.Write("max =" + numA);
+} 
+
+
+
+   
+
