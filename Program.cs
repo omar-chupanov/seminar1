8eabcfe63618c423e7198167b17eef55797e56a7
Omar <omar-chupanov@mail.ru>
2023-03-31 12:46:46 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Укажите число >");
+string value;
+value = Console.ReadLine();
+var numA = Convert.ToInt32(value);
+System.Console.Write("Укажите число >");
+value = Console.ReadLine();
+var numB = Convert.ToInt32(value);
+if (numA > numB)
+{
+System.Console.Write("max= "+ numA );
+}
+else
+{
+System.Console.Write ("max= "+ numB );
+}
