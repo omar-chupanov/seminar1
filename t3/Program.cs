5079839f2d226d0c79f81ce06f90ec2d1e93623f
Omar <omar-chupanov@mail.ru>
2023-03-31 12:39:11 +0300

    homework

---
+++ t3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Укажите число >");
+string value;
+value = Console.ReadLine();
+var num = Convert.ToInt32(value);
+if (num%2 ==0)
+{
+    System.Console.Write("Число  " + num + " является четным" );
+}
+else 
+{
+    System.Console.Write("Число " + num + " не является четным");
+}
